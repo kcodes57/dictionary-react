a416d1ac76881539b6b1440995a5c92bc666c88b
kcodes57 <kir652986@gmail.com>
2025-02-05 13:01:09 -0600

    created basic search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [searchWord, setSearchWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for the definition of ${searchWord}`);
+  }
+
+  function handleWordChange(event) {
+    setSearchWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleWordChange} />
+        <input type="submit" value="search" />
+      </form>
+    </div>
+  );
+}
