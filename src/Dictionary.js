1f9b8ef65281d341c591268fdbcce58031145080
kcodes57 <kir652986@gmail.com>
2025-02-08 14:04:54 -0600

    created definition of search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 import axios from "axios";
 
 export default function Dictionary() {
   let [searchWord, setSearchWord] = useState("");
+  let [results, setResults] = useState(null);
   let key = `441fcacb9dt020b37114da0ba3e3f5of`;
   let url = `https://api.shecodes.io/dictionary/v1/define?word=${searchWord}&key=${key}`;
 
   function handleReponse(response) {
-    console.log(response);
+    console.log(response.data);
+    setResults(response.data);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${searchWord}`);
+
     axios.get(url).then(handleReponse);
   }
 
@@ -27,6 +30,7 @@ export default function Dictionary() {
         <input type="search" onChange={handleWordChange} />
         <input type="submit" value="search" />
       </form>
+      <Results results={results} />
     </div>
   );
 }
