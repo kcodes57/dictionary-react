a6c6309b07efdb8114e7bfbd3a3a63d6cbac524c
kcodes57 <kir652986@gmail.com>
2025-02-08 14:44:07 -0600

    added part of speech

--- src/Results.js
+++ src/Results.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Results(props) {
   if (props.results) {
+    console.log(props.results);
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
         {props.results.meanings.map(function (meaning, index) {
-          return meaning[0].definition;
+          return meaning.partOfSpeech[0];
         })}
       </div>
     );
