6f5aa294fc0e5d4518ff413aaaa2a7234ab9f544
kcodes57 <kir652986@gmail.com>
2025-02-11 17:40:27 -0600

    adding audio pronunciation

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,1 +1,18 @@
-import React from "/.react";
+import React, { useState } from "react";
+
+import axios from "axios";
+
+export default function Phonetic(props) {
+  let [pronounce, setPronounce] = useState([]);
+
+  let word = props.word;
+  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  function handleReponse(response) {
+    setPronounce(response.data[0].phonetics);
+  }
+
+  axios.get(url).then(handleReponse);
+
+  console.log(pronounce);
+}
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
@@ -9,7 +10,10 @@ export default function Results(props) {
           <strong>{props.results.word}</strong>
         </h1>
 
-        <h2>{props.results.phonetic}</h2>
+        <h2>
+          {props.results.phonetic}
+          <Phonetic word={props.results.word} />
+        </h2>
         <h3>
           {props.results.meanings.map(function (meaning, index) {
             return (
