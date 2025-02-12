c9577d944e7e4c0ca1439dbbc70754fa0fb61615
kcodes57 <kir652986@gmail.com>
2025-02-12 13:10:21 -0600

    added coded by footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,29 @@ function App() {
       <header className="App-header">
         <Dictionary />
       </header>
+      <footer>
+        Coded by{" "}
+        <a href="https://github.com/kcodes57" target="_blank" rel="noreferrer">
+          Kirsten McIntyre
+        </a>{" "}
+        is open-sourced on{" "}
+        <a
+          href="https://github.com/kcodes57/dictionary-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://k-dictionary-react.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+      </footer>
     </div>
   );
 }
